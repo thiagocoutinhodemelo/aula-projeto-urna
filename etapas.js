10602e92d1672c3241713190d549e1e34bf5425d
thiagocoutinhodemelo <99850333+thiagocoutinhodemelo@users.noreply.github.com>
2022-12-07 22:58:56 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: 38111,
+            name: 'fulano de tal',
+            partido: 'ABC',
+            fotos:[
+                {url: '3811.jpg', legenda: 'vereador', small: false}
+            ]
+        },
+        {
+            numero: 77222,
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos:[
+                {url:'77222.jpg', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos:[
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
